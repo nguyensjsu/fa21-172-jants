1605767745a1263ff53e591492653e34b52f0996
Nimay Patel <60466954+Nimay-1709@users.noreply.github.com>
2021-11-29 00:38:47 -0800

    Update UserRepository.java

--- starbucks-jants/src/main/java/com/example/jants/UserRepository.java
+++ starbucks-jants/src/main/java/com/example/jants/UserRepository.java
@@ -1,6 +1,9 @@
 package com.example.jants;
 import org.springframework.data.jpa.repository.JpaRepository;
  
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, String> 
+{
+    User findByEmail(String email);   
+    User findByPassword(String password);
+
 }
